542d30dc9ecc8fce1b71e7699073171598be5fd5
utopia <pankajraj1278@gmail.com>
2022-11-28 09:41:06 +0530

    added a log statement

--- config/vars.js
+++ config/vars.js
@@ -1,6 +1,7 @@
 const path=require('path');
 require('dotenv').config();
 
+const GC_PRIVATE_KEY=process.env.GOOGLE_CLOUD_PRIVATE_KEY ?rocess.env.GOOGLE_CLOUD_PRIVATE_KEY.replaceAll('"',''):'';
 
 module.exports={
     env:process.env.NODE_ENV,
@@ -10,8 +11,8 @@ module.exports={
             process.env.MONGO_URI_TEST : process.env.MONGO_URI,
    },
    APPLICATION_ROOT:path.join(__dirname + '/..'),
-
-   GOOGLE_CLOUD_PRIVATE_KEY:process.env.GOOGLE_CLOUD_PRIVATE_KEY || '',
+ 
+   GOOGLE_CLOUD_PRIVATE_KEY: GC_PRIVATE_KEY,
    GOOGLE_CLOUD_PROJECT_ID:process.env.GOOGLE_CLOUD_PROJECT_ID || '',
    GOOGLE_CLOUD_CLIENT_EMAIL:process.env.GOOGLE_CLOUD_CLIENT_EMAIL || '',
    GOOGLE_CLOUD_BUCKET_NAME:process.env.GOOGLE_CLOUD_BUCKET_NAME || '',
