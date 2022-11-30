c2f4446d375fa7692b1ef9fab34fe557c00a6770
utopia <pankajraj1278@gmail.com>
2022-11-30 09:54:14 +0530

    Few bug fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,35 @@
 const jwt = require('jsonwebtoken');
+const Admin = require('../model/admin');
 
-module.exports= (req,res,next)=>{
+module.exports = async (req, res, next) => {
 
-    try{
-        const token=req.headers.authorization.split(' ')[1];
-        const decodedToken=jwt.verify(token, process.env.SECRET_KEY);
-        const email=decodedToken.email;
-        if(req.body.email && req.body.email !== email ){
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+        const email = decodedToken.email;
+
+        //check if email is not null
+        if(email){
+            const admin= await Admin.findOne({email:email},{status:1,_id:0});
+
+            //check if the admin is valid
+            //if not then notify the user
+            if(!admin){
+                return res.sendStatus(401);
+            }
+
+            //else let the request go ahead
+            return next();            
+        }
+
+        if (req.body.email && req.body.email !== email) {
             throw 'Invalid token.';
-        }else{
+        } else {
             next();
         }
-    }catch(err){
+    } catch (err) {
         res.status(401).json({
-            erorr:new Error('Invalid authorization token.').message
+            erorr: new Error('Invalid authorization token.').message
         });
     }
 }
